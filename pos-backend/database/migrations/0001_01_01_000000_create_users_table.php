6dd653849af1ecafe769185f5f6cf7c2717284a2
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-06-16 10:07:00 +0530

    refactor: enforce unique constraint on user name and remove unused session table migration

--- pos-backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ pos-backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps(); //make created_at & updated_at columns
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
@@ -27,14 +25,6 @@ return new class extends Migration
             $table->timestamp('created_at')->nullable();
         });
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
     }
 
     /**
