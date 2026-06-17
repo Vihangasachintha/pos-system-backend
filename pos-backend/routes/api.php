644e75289c412ea19d019f939bd81e623ac59d96
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-06-17 10:45:19 +0530

    feat: add login route and prepare for auth middleware integration

--- pos-backend/routes/api.php
+++ pos-backend/routes/api.php
@@ -2,11 +2,14 @@
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+// Route::middleware('auth-api')->group(function () {
+
+// });
 
 // This one line creates all 5 routes: index, store, show, update, destroy
 Route::apiResource('products', ProductController::class);
+Route::post('/login', [AuthController::class, 'login']);
