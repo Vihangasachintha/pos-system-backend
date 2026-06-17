75f958c53450506155844f5dbde1756f205d32bf
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-06-17 10:54:26 +0530

    fix: correct authentication middleware usage in API routes

--- pos-backend/routes/api.php
+++ pos-backend/routes/api.php
@@ -1,14 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
-// Route::middleware('auth-api')->group(function () {
-
-// });
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 // This one line creates all 5 routes: index, store, show, update, destroy
 Route::apiResource('products', ProductController::class);
