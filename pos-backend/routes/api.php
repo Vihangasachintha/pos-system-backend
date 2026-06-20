6964d9d51b2911c7f0d562644c0a6164be33f473
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-06-20 14:58:50 +0530

    feat: add stock movement routes to API

--- pos-backend/routes/api.php
+++ pos-backend/routes/api.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BrandController;
 use App\Http\Controllers\Api\CategoryController;
-use App\Http\Controllers\Api\VariantController;
 use App\Http\Controllers\Api\SupplierController;
+use App\Http\Controllers\Api\StockController;
 
 Route::middleware('auth:api')->group(function () {
     // This one line creates all 5 routes: index, store, show, update, destroy
@@ -15,8 +15,11 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/brands', [BrandController::class, 'store']);
     Route::post('/categories', [CategoryController::class, 'store']);
-    Route::post('/variants',   [VariantController::class,  'store']);
     Route::post('/suppliers',  [SupplierController::class, 'store']);
+
+    Route::get('stock-movements',       [StockController::class, 'index']);
+    Route::get('stock-movements/{stockMovement}', [StockController::class, 'show']);
+    Route::post('stock-movements',      [StockController::class, 'store']);
 });
 
 Route::post('/login', [AuthController::class, 'login']);
