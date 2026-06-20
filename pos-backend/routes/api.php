c0c6c3fbb3081484d98c73e99317e8f18aa545f7
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-06-20 21:38:18 +0530

    feat: add sales management routes to API

--- pos-backend/routes/api.php
+++ pos-backend/routes/api.php
@@ -8,13 +8,14 @@ use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\SupplierController;
 use App\Http\Controllers\Api\StockController;
 use App\Http\Controllers\Api\ExpenseController;
+use App\Http\Controllers\Api\SaleController;
 
 Route::middleware('auth:api')->group(function () {
     // This one line creates all 5 routes: index, store, show, update, destroy
     Route::apiResource('products', ProductController::class);
 
     Route::post('/logout', [AuthController::class, 'logout']);
-    
+
     Route::post('/brands', [BrandController::class, 'store']);
     Route::post('/categories', [CategoryController::class, 'store']);
     Route::post('/suppliers',  [SupplierController::class, 'store']);
@@ -28,6 +29,10 @@ Route::middleware('auth:api')->group(function () {
     Route::post('expenses',             [ExpenseController::class, 'store']);
     Route::put('expenses/{expense}',    [ExpenseController::class, 'update']);
     Route::delete('expenses/{expense}', [ExpenseController::class, 'destroy']);
+
+    Route::get('sales',          [SaleController::class, 'index']);
+    Route::get('sales/{sale}',   [SaleController::class, 'show']);
+    Route::post('sales',         [SaleController::class, 'store']);
 });
 
 Route::post('/login', [AuthController::class, 'login']);
